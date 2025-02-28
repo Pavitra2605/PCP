139f7d42247ab4f8b02885dff6def3a04bb91115
Pavitra P <fspavitra11@gmail.com>
2025-03-01 00:35:08 +0530

    Create String to Character Array Conversion.java

---
+++ String to Character Array Conversion.java
@@ -0,0 +1,24 @@
+import java.util.*
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner sc= new Scanner(System.in);
+	    String fn=sc.nextLine();
+	    String mn=sc.nextLine();
+	    
+	    char arr1[] = fn.toCharArray();
+	    char arr2[] = mn.toCharArray();
+	    
+	    
+	    System.out.println(Arrays.toString(arr1));
+	    System.out.println(Arrays.toString(arr2));
+	    
+	    for(int i=0; i<arr.length; i++)
+	    {
+	        System.out.println()
+	    }
+	    
+	}
+}
+
