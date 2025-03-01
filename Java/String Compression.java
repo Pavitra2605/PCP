221c9618f295896a2535de115cb9cf2e3b04691f
Pavitra P <fspavitra11@gmail.com>
2025-03-01 15:44:59 +0530

    Create String Compression.java

---
+++ Java/String Compression.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{  
+	    String a="aaabbbbbccdddaa";
+	    int c=1;
+	    String res = " ";
+	    for(int i=0; i < a.length(); i++)
+	    {
+	        if((i + 1) < a.length()&& a.charAt(i) == a.charAt(i + 1))
+	        {
+	            c++;
+	        }
+	        else
+	        {
+	            res = res + a.charAt(i);
+	            res = res + c;
+	            c = 1;
+	        }
+		
+	    }
+	    System.out.println(res);
+		
+	}
+}
