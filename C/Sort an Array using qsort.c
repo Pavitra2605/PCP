875d3932b5387091855fa563df542485de703c5d
Pavitra P <fspavitra11@gmail.com>
2025-03-01 22:14:29 +0530

    Create Sort an Array using qsort.c
    Sort an Array Using the qsort() Function and Custom Comparator

---
+++ C/Sort an Array using qsort.c
@@ -0,0 +1,18 @@
+# include<stdlib.h>
+
+int compare(const void *a,const void *b)
+{
+    return(*(int *)a - *(int *)b);
+}
+
+int main() {
+    int arr[]={5,7,90,33,22};
+    int size=sizeof(arr) / sizeof(arr[0]);
+    qsort(arr,size,sizeof(int),compare);
+    
+    printf("Sorted Array:");
+    for(int i=0; i<size; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
