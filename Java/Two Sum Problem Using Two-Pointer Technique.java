ba2564b46f11fa77a898fd3a1ffe85424e92843a
Pavitra P <fspavitra11@gmail.com>
2025-03-01 21:49:55 +0530

    Create Two Sum Problem Using Two-Pointer Technique.java
    Find two numbers in a sorted array that add up to a given target.
    If such numbers exist, the program prints their indices. Otherwise, it outputs "No SOLUTION IS FOUND.

---
+++ Java/Two Sum Problem Using Two-Pointer Technique.java
@@ -0,0 +1,32 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr=[2,7,11,15]
+		int target=17;
+		int left=0;
+		int right=arr.length -1;
+		int[] result= new int[]{};
+		while(left < right) {
+		    int sum = arr[left] + arr[right];
+		    
+		    if( sum== target){
+		        result = new int[]{left,right};
+		        break;
+		    }
+		    else if(sum < target){
+		        left++;
+		    }
+		    else{
+		        right --;
+		    }
+		}
+		if(result.length == 2){
+		    System.out.println("Indices :[" +result[a]+", "+result[1] + "]")
+		    }
+		    else{
+		        System.out.println("No SOLUTION IS FOUND")
+		        
+		    }
+		
+	}
+}
